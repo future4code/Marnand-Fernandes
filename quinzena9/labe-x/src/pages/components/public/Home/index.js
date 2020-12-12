fb5103463f320cd411f45bb3548f4a73ab2bf6fa
marnand <marnandf@gmail.com>
2020-12-12 20:11:40 -0300

    layout da home iniciado

--- quinzena9/labe-x/src/pages/components/public/Home/index.js
+++ quinzena9/labe-x/src/pages/components/public/Home/index.js
@@ -1,23 +1,47 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import Container from './styled'
+import rocket from '../../../../assets/icons/foguete.svg';
+import planet from '../../../../assets/icons/planetas.svg';
+import logo from '../../../../assets/icons/futurama.svg';
 
 const Home = () => {
   const history = useHistory();
 
   const goToTripsList = () => {
-    history.push("/trips/list");
-  }
+    return history.push("/trips/list");
+  };  
 
   const goToLogin = () => {
-    history.push("/login");
-  }
+    return history.push("/login");
+  };
 
   return (
-    <div>
-      <h1>Home Page</h1>
+    <Container>
+      <header>
+        <div>
+          <img src={logo} />
+        </div>
+      </header>
+      <main>
+        <h1>Valores incluem</h1>
+        <ul>
+          <li><img src={planet} /> 7 noites de hospedagem flutuante</li>
+          <li><img src={rocket} /> Translado de chegada e saída em velocidade de dobra </li>
+        </ul>
+      </main>
+      <article>
+        <section>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </section>
+      </article>
+
       <button onClick={goToTripsList}>Lista de viagens</button>
       <button onClick={goToLogin}>Acesso Restrito</button>
-    </div>
+    </Container>
   );
 };
 
---
+++ quinzena9/labe-x/src/pages/components/public/Home/styled.js
@@ -0,0 +1,76 @@
+import styled from "styled-components";
+import spaceTrip from '../../../../assets/img/space-trip.png';
+
+const Container = styled.div`  
+  header {
+    background: url(${spaceTrip}) no-repeat;
+    background-size: cover;
+
+    width: 100%; 
+    height: 70vh;
+  }
+
+  header > div {
+    width: 15%;
+  }
+
+  div > img {
+    margin: 10% 0 0 10%;
+    width: 100%;
+  }
+
+  main {
+    text-align: center;
+
+    color: #fff;
+    background: rgb(3,14,105);
+    background: linear-gradient(90deg, rgba(3,14,105,.6) 0%, rgba(54,102,241,1) 35%, rgba(21,247,248,.6) 100%);
+  }
+
+  h1 {
+    padding: 20px 0;
+  }
+
+  ul {
+    padding: 0 0 20px 0;
+  }
+
+  li {
+    display: inline;
+    padding: 16px;
+  }
+
+  li > img {
+    width: 40px;
+  }
+
+  article > section {
+    display: flex;
+    flex-wrap: wrap;
+
+    width: 100%;
+  }
+
+  section > div {
+    width: 200px;
+    height: 200px;
+    border: 1px solid #000;
+    width: 25%;
+  }
+
+  @media (max-width: 800px){
+    header > div {
+      width: 20%;
+    }
+
+    section > div{
+      width: 100%;
+    }
+
+    li {
+      display: block;
+    }
+  }
+`;
+
+export default Container;
